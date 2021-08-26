a952472f8ca5064ee221aeb4c8240f6e35dbacc9
Gora <it.is.mr.lag@gmail.com>
2021-08-26 17:22:14 +0500

    instrument multiprocessing

--- multiprocess.py
+++ multiprocess.py
@@ -1,5 +1,5 @@
 from multiprocessing import Queue, Process, Pipe
-import time as tm
+import time
 import os
 
 """
@@ -9,123 +9,121 @@ which acts as a buffer (all interaction is carried out through it,
 so it must be fast, and it must have means of protection against possible errors due to asynchronous)
 """
 
-class NewProcess:
+class Informator:
+    """
+    class for creating another process for reading information from it
+    *should be used with Intermediary
+    """
+
+    def __init__(self, func):
+        """
+        @param func: <type>: function - informator-function
+        """
+        self.func = func
+        self.q = Queue(maxsize=2)
+        self.proc = Process(target=self.func, args=(self.wrapper,))
+        self.proc.start()
+
+    def wrapper(self, information):
+        """
+        @param information: <type>: anything - information for sending in the main process
+        """
+        if self.q.empty() is True:
+            self.q.put(information)
+
+    def reader(self):
+        if self.q.empty() is True:
+            return self.q.get()
+
+
+class Messenger:
+    def __init__(self, func):
+        """
+        @param func: <type>: function - sent-function
+
+        ############### EXAMPLE ###################
+        def sender(func):
+            while True:
+                info = func()
+                print(info)
+
+        send = Messenger(sender)
+        while True:
+            info_give = "hello mr.lag"
+            send.send(info_give)
+
+        """
+        self.func = func
+        self.q = Queue(maxsize=2)
+        self.proc = Process(target=self.func, args=(self.wrapper,))
+        self.proc.start()
+
+    def wrapper(self):
+        if self.q.empty() is not True:
+            return self.q.get()
+
+    def send(self, information):
+        if self.q.empty() is True:
+            self.q.put(information)
+
+
+class Intermediary:
     """
     creating of the 'second-helper' process for connecting 'third-context' process with 'first-main'
+
+    ############### EXAMPLE ####################
+    -------------> INFORM FUNC <-------------------
+    def Func1(func):                            # 'in this argument would be given Informator.wrarpper'
+        print(os.getpid(), ' - Function')
+        l = 0
+        while True:
+            print(l, ' - func')
+            time.sleep(0.1)
+            l += 1
+            func(l)                              #  'required template for the send function'
+
+    -------------> MAIN PROCESS <-----------------------
+    test = Intermediary(Func1, 1)                #  'initialization'
+    print(os.getpid(), ' - Main')
+    while True:                                  #  'main loop'
+        time.sleep(2)
+        print(test.reader(), ' - final info')    #  'reading info from informator-function  '
     """
-    # todo the same - delay for basic information to appear in the 'third-helper' process
 
-    def __init__(self):
-        self.qp = Queue(maxsize=2)
-        self.q = Queue(maxsize=2)
-        self.proc = Process(target=self.sender1)
-        self.proc.start()
-        tm.sleep(1)  # todo the same
-
-    def sender1(self):
-        info = None
-        print('proc - second', os.getpid())
-
-        class NewProcess1:
-            def __init__(self):
-                """
-                creating 'third-context' process (getter information from module as GPS)
-                """
-                self.q1 = Queue(maxsize=2)
-                self.proc = Process(target=self.sender)
-                self.proc.start()
-                tm.sleep(1)  # todo the same
-
-            def sender(self):
-                print('proc - third', os.getpid())
-                l = 0
-                while True:
-                    print(l, ' - third layer')
-                    tm.sleep(1)  # todo the same (the main delay)
-                    l += 1
-                    if self.q1.empty():
-                        self.q1.put([l, 23, 54.886958688])
-
-        proc_inner = NewProcess1()
-        while True:
-            if proc_inner.q1.empty() == False:
-                info = proc_inner.q1.get()
-                print(info[0], ' - second layer')
-
-            if self.qp.empty() is not True:
-                out = self.qp.get()
-                if self.q.empty():
-                    self.q.put(info)
-
-
-#####################test############################################
-# proc = NewProcess()
-# print('proc - first-final', os.getpid())
-# """
-# it is the first-main process (this process want get actual information from 'third-context' process)
-# """
-# while True:
-#     tm.sleep(4)
-#     if proc.qp.empty():
-#         proc.qp.put('give')
-#         tm.sleep(0.1)  # delay for getting information from 'second-helper' process
-#     if proc.q.empty() is not True:
-#         print(proc.q.get(), ' - first-final layer <<<---<<<<--------<<<<-------<<<<---')
-#####################test############################################
-
-
-############## just for tests #######################################
-class Intermediary:
-    def __init__(self, all_funcs, delay):
-        self.all_funcs = all_funcs
+    def __init__(self, func, delay):
+        """
+        @param func: <type>: function - informator-function
+        @param delay: <type>: int - delay before processes initialization
+        """
+        self.all_funcs = func
         self.keys = []
-        self.info_q = Queue(maxsize=2)
-        self.main_q = Queue(maxsize=2)
-        self.proc = Process(target=self.creator, args=(all_funcs,))
+        self.info_q = Queue(maxsize=2)         # queue for signal 'give'
+        self.main_q = Queue(maxsize=2)         # queue for received info
+        self.proc = Process(target=self.creator, args=(func,))
         self.proc.start()
-        tm.sleep(delay)
+        time.sleep(delay)
 
-    def creator(self, funcs):
+    def creator(self, func):
+        print(os.getpid(), ' - Intermediary')
 
-        class Informator:
-            def __init__(self, func):
-                self.func = func
-                self.q = Queue(maxsize=2)
-                self.proc = Process(target=self.func, args=(self.wrapper,))
-                self.proc.start()
-
-            def wrapper(self, information):
-                if self.q.empty() is not True:
-                    self.q.put(information)
-
-        name = 0
-        library = {}
-        mean = {}
-        for i in funcs:
-            library[name] = Informator(i)
-            mean[name] = None
+        info = None
+        module = Informator(func)
 
         while True:
-            if self.info_q.empty() is not True:
+
+            if self.info_q.empty() is not True:  # sender in the main process
                 out = self.info_q.get()
                 if self.main_q.empty():
-                    self.main_q.put(mean[out])
+                    self.main_q.put(info)
 
-            for i in library:
-                if library[i].q.empty() is not False:
-                    mean[i] = library[i].q.get()
+            if module.q.empty() is not True:     # reader from Inforamator
+                info = module.q.get()
 
-
-# чтение добавить
-# проверить
-# удаление добавить
-
-
-def exemp(func):  # тестовая выборка)
-    l = 0
-    while True:
-        tm.sleep(1)
-        l += 1
-        func(l)
-
-############## just for tests #######################################
+    def reader(self):
+        if self.info_q.empty() is True:
+            self.info_q.put('give')
+            time.sleep(0.1)
+        if self.main_q.empty() is not True:
+            return self.main_q.get()
+        else:
+            return 'ERROR'
